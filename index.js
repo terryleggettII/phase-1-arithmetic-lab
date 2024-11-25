5bcdf6e9a35f0d89ab3da399c6c12b4847438b1b
Terry Leggett II <terryleggettjr@gmail.com>
2024-11-25 10:30:21 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31
+let num2 = 2
+function multiply(num1, num2) {
+    return num1 * num2
+}
 
-// Write your code here
+function random(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function mod(num3, num4) {
+    return num3 % num4
+}
+let result = mod(13, 3);
+console.log(result);
+
+function max (firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        return firstNumber
+    } else {
+        return secondNumber
+    }
+}
+console.log(max(20,10));
--- test/indexTest.js
+++ test/indexTest.js
@@ -8,25 +8,36 @@ describe('index.js', function() {
     it("is an equation whose multiplied results will equal 62", function() {
       expect(num1).to.be.a('number')
       expect(num2).to.be.a('number')
-      expect(multiply).to.eq(62)
+      expect(multiply(num1, num2)).to.eq(62);
     });
   });
   describe('random', function() {
     it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
+      const result = random(100)
+      expect(result).to.be.a('number');
+      expect(result).to.be.greaterThan(0);
     });
   });
   describe('mod', function() {
     it("is an equation that calculates a remainder that is equal to 4", function() {
-      expect(num3).to.be.a('number')
-      expect(num4).to.be.a('number')
-      expect(mod).to.eq(4)
+      // Define the numbers you want to use in the test
+      let num3 = 13; // Example number
+      let num4 = 9;  // Use 9 so that 13 % 9 = 4
+  
+      // Call the mod function with these numbers
+      let result = mod(num3, num4);
+  
+      // Check that the variables are numbers
+      expect(num3).to.be.a('number');
+      expect(num4).to.be.a('number');
+  
+      // Verify that the mod function returns the expected result
+      expect(result).to.eq(4);
     });
   });
   describe('max', function() {
     it("will return 20 as the highest number in the set", function() {
-      expect(max).to.eq(20)
+      expect(max(10, 20)).to.equal(20);
     });
   });
 });
